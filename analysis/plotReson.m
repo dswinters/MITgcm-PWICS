c33a9a8a8871827fd23ec1b95ebf93bc3442d010
Dylan Winters <dylan.winters@oregonstate.edu>
2021-03-10 08:05:47 -0800

    initial commit

---
+++ analysis/plotReson.m
@@ -0,0 +1,80 @@
+function plotReson(theta,kTopo)
+
+% TODO: This was adapted from a version that looped over different corrugation
+% amplitudes. Need to test on standard theta/kTopo parameter space.
+
+fs = 14; fn = 'times';
+lam = 2*pi./kTopo;
+dx_inner = 5e3; %2.e3;
+dy_inner = 5e3; %2.e3;
+om = 1.36*1e-4;
+
+tags = {'_20km'};
+
+datt = [];
+aflxBC = {[]};
+aflx = {[]};
+yf = {};
+
+for i = 1:length(theta)
+    for j = 1:length(kTopo)
+        % Load run data
+        thetaPrefix = sprintf('theta%3.2f_',theta); % File prefix for theta
+        kTopoPrefix = sprintf('kTopo%.8f_',kTopo(j)); % File prefix for kTopo
+        rname = sprintf('run_%s%s',thetaPrefix,kTopoPrefix(1:end-1));
+        load(fullfile(rdir,'runs',rname,'corrugation_params.mat'),'xSin1')
+        datt = rdmnc(fullfile(rdir,'runs',rname,'outs_sn.*'),'T','iter');
+        fluxFile = fullfile(rdir,'analysis','calcFluxBC',[rname '_flux_wave_avg_end.mat']);
+
+        if exist(fluxFile,'file')
+            fl = load(fluxFile);
+            if j==1
+                gridm = rdmnc(fullfile(rdir,'runs',rname,'grid*'));
+                yf{i} = fl.yc(find(gridm.Depth(1,:)==0,1,'last')) + [0 100e3];
+            end
+            indy = fl.yc >= yf{i}(1) & fl.yc <= yf{i}(2);
+
+
+            % X location where we computed fluxes (70km after end of sine topog)
+            [~,indx] = min(abs(fl.xc-xSin1+70e3));
+
+            aflxBC{i}(j) = squeeze(nansum(fl.upbcNz(indx,indy,:)*dy_inner,2));
+            aflx{i}(j) = squeeze(nansum(fl.upz(indx,indy,:)*dy_inner,2));
+        else
+            aflxBC{i}(j) = nan;
+            aflx{i}(j) = nan;
+        end
+        fprintf('\r%s [%d of %d]',fluxFile,j,length(kTopo))
+    end
+end
+fprintf('\n')
+save plotReson.mat aflxBC aflx indx datt yf
+
+figure('position',[0 0 1065 400],'color','w','paperpositionmode','auto');
+
+% subplot(2,2,3)
+pl1 = [];
+pl2 = [];
+cols = get(groot,'DefaultAxesColorOrder');
+clf
+tags = {'20km'};
+
+for i = 1:length(aflx)
+    subplot(1,2,1); hold on
+    pl1(i) = plot(lam,aflxBC{i}(end,:),'-','linewidth',1,'color',cols(i,:)); grid on
+    xlabel('\lambda Topo')
+    ylabel('Baroclinic Flux (Along-shelf/incident)')
+    set(gca,'fontsize',8)
+
+    % subplot(2,2,4)
+    subplot(1,2,2); hold on
+    pl2(i) = plot(lam,aflx{i}(end,:),'-','linewidth',1,'color',cols(i,:)); grid on
+    xlabel('\lambda Topo')
+    ylabel('Total Flux (Along-shelf/Incident)')
+    set(gca,'fontsize',8)
+end
+
+hl = legend(pl1,tags,'location','east','interpreter','none');
+hl = legend(pl2,tags,'location','east','interpreter','none');
+
+print('-dpng','pwics_resonance.png')
